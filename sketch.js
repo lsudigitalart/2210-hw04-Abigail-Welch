4ed19225183bf9a01e0a3feb084a4db1720a0439
Derick Ostrenko <dostrenko@lsu.edu>
2024-09-30 15:15:15 -0500

    made a branch

--- sketch.js
+++ sketch.js
@@ -4,7 +4,12 @@ difficulty = 1
 function setup () {
     createCanvas (700,300);
 
+    rx = 50;
+    ry = height-50;
+
 }
+
+let rx, ry;
 
 function draw () {
 
@@ -32,21 +37,21 @@ function draw () {
     fill (255, 255, 255);
     stroke (114, 163, 85);
     strokeWeight (3)
-    rect(mouseX, 250, 40, 40);
+    rect(rx, ry, 40, 40);
 
 
-    if (mouseX < 0){
-        mouseX = mouseX + 10;
-    }
-    if (mouseX > width){
-        mouseX = mouseX - 20;
-    }
-    if (mouseY < 0){
-        mouseY = mouseY + 10;
-    }
-    if (mouseY > height){
-        mouseY = mouseY - 20;
-    }
+    // if (mouseX < 0){
+    //     mouseX = mouseX + 10;
+    // }
+    // if (mouseX > width){
+    //     mouseX = mouseX - 20;
+    // }
+    // if (mouseY < 0){
+    //     mouseY = mouseY + 10;
+    // }
+    // if (mouseY > height){
+    //     mouseY = mouseY - 20;
+    // }
 
 
   fill(0)
@@ -67,5 +72,55 @@ function draw () {
     text (mouseX, 100, 40)
     text (mouseY, 100, 20)
 
+
+    if(keyIsPressed) {
+        if (key == "a") {
+            rx--;
+        }
+        if (key == "d") {
+            rx++;
+        }
+        if (key == "w") {
+            ry--;
+        }
+        if (key == "s") {
+            ry++;
+        }
+        if (key == " ") {
+            jump();
+        }
+    }
+
+    if(jumped) {
+        if(frameCount % 6 == 0) {
+            jumpTime++;
+        }
+        if(jumpTime > 5) {
+            if(rx < 230) {
+                if(ry < height - 50) {
+                    ry = ry + 5;
+                }
+            }
+            if(rx > 230 && rx < 375) {
+                if(ry < height - 50 - 100) {
+                    ry = ry + 5;
+                }
+            }
+
+            if(rx > 375 && rx < 520) {
+                if(ry < height - 50 - 150) {
+                    ry = ry + 5;
+                }
+            }
+        } 
+    }
 }
 
+let jumpTime = 0;
+let jumped = false;
+
+function jump() {
+    ry = ry - 15;
+    rx = rx + 2.5;
+   jumped = true;
+}
