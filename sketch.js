f7e3a48d032721d1bb7a974364ee1d9cc599a1f9
“Abigail-Welch” <awelc17@lsu.edu>
2024-10-14 11:53:39 -0500

    assignment 04 update

--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
+//i figured this needs to just be turned in and as finished as i can make it for midterm grades.
+//it works for the most part, but there's just one issue with where the block lands for each of the jump platforms
 
-difficulty = 1
+
+difficulty = 0
 
 function setup () {
     createCanvas (700,300);
 
+    rx = 50;
+    ry = height-50;
+
 }
+
+let rx, ry;
 
 function draw () {
 
@@ -12,8 +20,9 @@ function draw () {
 
     fill (115, 191, 77);
     noStroke();
-    rect (680, -2, 100);
+    rect (680, -2, 100, 100);
 
+    //jump platforms
 
     fill (179, 151, 70);
     noStroke ()
@@ -29,43 +38,101 @@ function draw () {
     fill (179, 151, 70);
     rect (520, 95, 100, 10);
 
+    //character
+
     fill (255, 255, 255);
     stroke (114, 163, 85);
     strokeWeight (3)
-    rect(mouseX, 250, 40, 40);
+    rect(rx, ry, 40, 40);
 
+    // game boarders 
 
-    if (mouseX < 0){
-        mouseX = mouseX + 10;
+     if (rx < 0){
+        rx = rx + 10;
+     }
+     if (rx > width){
+        rx = rx - 20;
+     }
+     if (ry < 0){
+        ry = ry + 10;
+     }
+     if (ry > height){
+        ry = ry - 20;
     }
-    if (mouseX > width){
-        mouseX = mouseX - 20;
-    }
-    if (mouseY < 0){
-        mouseY = mouseY + 10;
-    }
-    if (mouseY > height){
-        mouseY = mouseY - 20;
-    }
 
+    //score 
 
-  fill(0)
-  textSize(25);
-  text(difficulty, 10, 25)
-
-
-    
-
-
-
-
+    fill(0)
+    textSize(25);
+    text(difficulty, 10, 25)
 
     //mouse location
-    stroke (0);
-    strokeWeight (1)
-    textSize (15)
-    text (mouseX, 100, 40)
-    text (mouseY, 100, 20)
+
+    //  stroke (0);
+    //  strokeWeight (1)
+    //  textSize (15)
+    //  text (mouseX, 100, 20)
+    // text (mouseY, 100, 40)
+
+    //key controls
+
+    if(keyIsPressed) {
+        if (key == "a") {
+            rx--;
+        }
+        if (key == "d") {
+            rx++;
+        }
+        if (key == " ") {
+            jump();
+        }
+    }
+
+    //platform jumps
+
+    if(jumped) {
+        // if(frameCount % 10 == 0) {
+        //     jumpTime++;
+        // }
+        // if(jumpTime > 5) {
+            if(rx < 230) {
+                if(ry < 190 - 43 && ry > 5) {
+                    ry = ry + 5;
+                }
+            }
+            if(rx > 230 && rx < 375) {
+                if(ry < 137 - 43) {
+                    ry = ry + 5;
+                }
+            }
+
+            if(rx > 375 && rx < 520) {
+                if(ry < 95 - 43) {
+                    ry = ry + 5;
+                }
+            }
+        // } 
+    } 
+
+print(rx, ry, jumpTime);
 
 }
 
+    let jumpTime = 0;
+    let jumped = false;
+
+    function jump() {
+        ry = ry - 15;
+        rx = rx + 2.5;
+    jumped = true;
+
+    //goal contact
+
+    if (rx + 100 > 680 && ry + 40 > -2 && ry < -2 + 40){
+        console.log ('bumped');
+        rx = width - 650;
+        ry = height - 50
+        difficulty += 1;
+    } 
+
+    }
